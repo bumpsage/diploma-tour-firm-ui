7d3cbb9af3d18c72d0cbc4ab5757bf7eb3608d0e
bumpsage <vlad380660623668@gmail.com>
2022-05-31 03:11:25 +0300

    Intermid commit

--- src/app/services/reference.service.ts
+++ src/app/services/reference.service.ts
@@ -1,5 +1,5 @@
 
 export class ReferenceService {
   // public static API_URL: string = 'http://localhost:8080/api';
-  public static API_URL: string = 'https://summer-practice-tour-firm.herokuapp.com/api';
+  public static API_URL: string = 'https://diploma-tour-firm.herokuapp.com/api';
 }
